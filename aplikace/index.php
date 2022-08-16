d77ed8de162967255a867c4610e0f35b476b496b
Bohatová Zuzana <941-bohatovz@users.noreply.localhost>
2022-08-16 15:56:44 +0000

    Upload New File

---
+++ aplikace/index.php
@@ -0,0 +1,43 @@
+<?php
+    
+    function main()
+    {
+        if (!isset($_SERVER['PATH_INFO']))
+        {
+            header('Location: index.php/', false, 303);
+            return;
+        }
+        
+        $url = explode('/', substr($_SERVER['PATH_INFO'], 1));
+       
+        switch ($url[0])
+        {
+            case "":
+            case "uvod":
+                require_once __DIR__ . '/uvod.php';
+                break;
+            case "grafy":
+                require_once __DIR__ . '/grafy.php';
+                break;
+            case "hardware":
+                require_once __DIR__ . '/hardware.php';
+                break;
+            case "site":
+                require_once __DIR__ . '/site.php';
+                break;
+            case "ukladani-dat":
+                require_once __DIR__ . '/ukladani-dat.php';
+                break;   
+            case "ciselne-soustavy":
+                require_once __DIR__ . '/ciselne-soustavy.php';
+                break; 
+            default:
+                error(404, "Page not found");
+                break;
+        }
+    }
+
+    require_once __DIR__ . '/header.php';
+    main();
+    require_once __DIR__ . '/footer.php';
+?>
