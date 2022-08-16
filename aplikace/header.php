b5010269f49007ab015bde8369f6a909cdbb60b8
Bohatová Zuzana <941-bohatovz@users.noreply.localhost>
2022-08-16 15:56:27 +0000

    Upload New File

---
+++ aplikace/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Začínáme s informatikou</title>
+        <link rel="stylesheet" href="/~86974125/informatika/styly.css" type="text/css">
+
+    </head>
+    <body>
+        <header>
+            <h1 id="hlavni-nadpis">Začínáme s informatikou</h1>
+        </header>
+    <nav>
+    <ul>
+        <li><a href="uvod">Úvod</a></li>
+        <li><a href="grafy">Grafy</a></li>
+        <li><a href="hardware">Hardware</a></li>  
+        <li><a href="ukladani-dat">Ukládání dat</a></li>
+        <li><a href="ciselne-soustavy">Číselné soustavy</a></li>
+        <li><a href="site">Sítě</a></li>
+    </ul>
+    </nav>
+        
