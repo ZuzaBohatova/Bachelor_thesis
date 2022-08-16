20fe143d89b7e46804845b3f4bd82f0cd324be3a
Bohatová Zuzana <941-bohatovz@users.noreply.localhost>
2022-08-16 15:57:43 +0000

    Upload New File

---
+++ aplikace/uvod.php
@@ -0,0 +1,11 @@
+<h2>Úvod</h2>
+<p>Vítáme vás ve výukovové aplikaci Začínáme s informatikou.
+    <br>Aplikace má sloužit převážně dětem na druhém stupni základní školy. Její obsah byl přizpůsoben podle RVP od MŠMT.
+    Obsahuje jak teorii, tak i hry, které prověří zábavnou formou vaše znalosti.</br>
+<hr>
+    Výukou vás budou provádět Robíci - bla, dla, mla, a kla.
+    Tak hurá do toho!!!
+
+</p>
+
+<img src="https://webik.ms.mff.cuni.cz/~86974125/informatika/rob01.svg" alt="Robot" width="100">
