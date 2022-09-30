8d796fe55798adddf022c8e3274cc79588134fd9
Zuzana Bohatova <bohatovz@u-pl13.ms.mff.cuni.cz>
2022-09-30 12:11:23 +0200

    update hra3

--- ZabavnaInformatika/aplikace/hra3Novy.php
+++ ZabavnaInformatika/aplikace/hra3Novy.php
@@ -1,64 +1,38 @@
 <script>
-    document.getElementById("menu").style.border = "5px solid rgb(27, 54, 143)";
-  
+  document.getElementById("menu").style.border = "5px solid rgb(27, 54, 143)";
+  let kolo = 1;
 
-  function checkHra3(){
+  function checkAns() {
     let correctAns = ["magPaska","cd", "ssd","hdd","usb"];
-    let correctPic = ["2","3","4","1","5"];
-    let ans = ["pamet1", "pamet2", "pamet3", "pamet4", "pamet5"];
-    let pic = ["ansObr1", "ansObr2", "ansObr3", "ansObr4", "ansObr5"];
-    let missPic = 0;
-    let missAns = 0;
-    let chybyAns = "Chyboval jsi v otázkách:";
-    let chybyPic = "Špatně jsi přiřadil obrázky:";
-
-    for(let i = 0; i < 5; i++){
-      if(correctAns[i] != document.getElementById(ans[i]).value){
-        chybyAns += " "+(i+1);
-        ++missAns; 
+    for(let i = 0; i < correctAns.length; i++){
+      if((kolo-1) == i){
+        document.getElementById(correctAns[i]).onclick = function(){document.getElementById(id).style.border = "2px solid green";}
       }
-      if(correctPic[i] != document.getElementById(pic[i]).value){
-        chybyPic += " "+(i+1);
-        ++missPic; 
+      else {
+        document.getElementById(correctAns[i]).onclick = function(){document.getElementById(id).style.border = "2px solid red";}
       }
-
-    } 
-    if(missAns > 0){
-      output.innerHTML = "<br>"+chybyAns;
     }
-    if(missPic > 0){
-      output.innerHTML += "<br>"+chybyPic;
-    }
-    if((missPic+missAns) == 0){
-      alert("Neudělal jsi jedinou chybu dobrá práce!!!")
-      output.innerHTML = "Výborně, jen tak dál"
-    }
-    else{
-      alert("Nějaké chyby tam byly, koukni kde, zkus to znovu!")
-    }
-    
   }
 
   function allOptions(){
-    var options = `<option value="free"></option>
-    <option value="hdd">Pevný disk (HDD)</option>
-    <option value="usb">USB flash disk</option>
-    <option value="ssd">SSD</option>
-    <option value="magPaska">Magnetická páska</option>
-    <option value="cd">CD</option>
-    </select><br>`;
+    var options = `<div id="ans"><button id="hdd">Pevný disk (HDD)</button>
+      <button id="usb" onclick="checkAns('true', 'usb')">USB flash disk</button>
+      <button id="ssd">SSD</button>
+      <button id="magPaska">Magnetická páska</button>
+      <button id="cd">CD</button></div>`;
     return options;
   }
 
   function zacniHru(){
-    textHra3.innerHTML = `<br><a id="zpetUD" href="ukladani-dat">Zpět na teorii</a><h2>Přiřazovačka</h2><br><form id="formHra3" name="formHra3" action="hra3" method="post">
-    <label for="pamet1">Používám magnetický zápis. Jsem ideální pro archivaci velkého množství dat, ale pomalu už se blížím do důchodu. Kdo jsem?</label>
-    <select id="pamet1" name="pamet1">`+allOptions()+`
-    <label for="ansObr1">Obrázek číslo:</label>
-    <input type="number" id="ansObr1" name="ansObr1" min="1" max="5" required><br>
-    </form><br>`;
-    document.getElementById("hra3robot").style.visibility = "hidden";
+    textHra3.innerHTML = `<br><a id="zpetUD" href="ukladani-dat">Zpět na teorii</a><h2>Přiřazovačka</h2><p class="ot" id="ot1">Používám magnetický zápis. 
+    Jsem ideální pro archivaci velkého množství dat, ale pomalu už se blížím do důchodu. Kdo jsem a jak asi vypadám?</label>
+      </p>`+allOptions()+"<br>";
     dalsiOtazka();
+    textHra3.innerHTML += `<div id="hra3Obr"><input type="image" src="../../pictures/hdd.png" alt="Pevný disk" width="180"/>
+      <input type="image" src="../../pictures/magPaska.png" alt="Magnetická páska" width="180"/>
+      <input type="image" src="../../pictures/cd.png" alt="CD" width="180"/>
+      <input type="image" src="../../pictures/ssd.jpg" alt="SSD" width="180"/>
+      <input type="image" src="../../pictures/usb.png" alt="USB flash disk" width="180"/></div>`;
     
   }
 
@@ -71,20 +45,23 @@
 <style type="text/css">
 #prirazovacka {
   display:grid;
-  grid-template-columns: auto  20%;
+  grid-template-columns: auto 30%;
+  grid-template-rows: 60% auto;
   grid-template-areas:
     "text robot"
+    "obrazky dalsi";
+
 }
 
-#formHra3 {
+.ot {
   font-size: 18px;
 }
 #textHra3 {
    grid-area:text;
-}
 
+}
 #hra3Obr {
-  visibility: hidden;
+  
 }
 
 #speech-bubble { 
@@ -102,9 +79,9 @@
    width: 0;
    height: 0;
    border-top: 13px solid transparent;
-   border-left: 55px solid #A7C7E7;
+   border-left: 90px solid #A7C7E7;
    border-bottom: 13px solid transparent;
-   margin: 50px -80px 25px 0px;
+   margin: 80px -100px 30px 0px;
 }
 
 #zpetUD {
@@ -116,6 +93,7 @@
 }
 
 #dalsiBut {
+    justify-items: center;
   background-color: #A7C7E7;
   border: 1px solid #A7C7E7;
   border-radius:20px;
@@ -125,6 +103,11 @@
   display: inline-block;
   font-size: 16px;
   float: right;
+}
+
+#dalsi {
+  grid-area: dalsi;
+  place-items: center;
 }
 
 #zacniHru {
@@ -139,42 +122,6 @@
   float: right;
 }
 
-#hra3 {
-    grid-area:kviz;
-}
-
-#formReset {
-  background-color: yellow;
-  border: 1px solid navy;
-  border-radius:10px;
-  color: navy;
-  padding: 15px 32px;
-  text-align: center;
-  display: inline-block;
-  font-size: 16px;
-}
-
-#kontrola {
-  background-color: #A7C7E7;
-  border: 1px solid navy;
-  border-radius:10px;
-  color: navy;
-  padding: 15px 32px;
-  text-align: center;
-  display: inline-block;
-  font-size: 16px;
-}
-
-ol {
-    padding: 20px; 
-    list-style-type: decimal;
-
-}
-ol li {
-    float: left;
-    margin: 0 10px;
-    padding: 0 10px;
-}
 
 </style>
 <div id="prirazovacka">
@@ -191,16 +138,9 @@ Vylučovací metodou se určitě dostaneš ke správné odpovědi.
 <br>Každá otázka má jen jednu odpověď a každá odpověď je použita jen jednou. </i></p></div>
 <br><button id="zacniHru" onclick="zacniHru()"><strong>Začni hru</strong></button>
 </div>
-<img id="hra3robot" src="../../pictures/rob03.png" alt="Robot3" width="250">
-
-<div id="hra3Obr">
-<ol>
-<li><img src="../../pictures/hdd.png" alt="Pevný disk" width="180"></li>
-<li><img src="../../pictures/magPaska.png" alt="Magnetická páska" width="180"></li>
-<li><img src="../../pictures/cd.png" alt="CD" width="120"></li>
-<li><img src="../../pictures/ssd.jpg" alt="SSD" width="180"></li>
-<li><img src="../../pictures/usb.png" alt="USB flash disk" width="150"></li>
-</ol></div>
+<div id="hra3robot">
+<img id="hra3robotUvod" src="../../pictures/rob03.png" alt="Robot3" width="300">
+</div>
 <div id="dalsi"></div>
 
 
@@ -210,28 +150,28 @@ Vylučovací metodou se určitě dostaneš ke správné odpovědi.
 <form name="formHra3" action="hra3" method="post">
   <label for="pamet1">1. Používám magnetický zápis. Jsem ideální pro archivaci velkého množství dat, ale pomalu už se blížím do důchodu. Kdo jsem?</label>
   <select id="pamet1" name="pamet1">
-    <script>allOptions()</script>
+    <script>allbuttons()</script>
   <label for="ansObr1">Obrázek číslo:</label>
   <input type="number" id="ansObr1" name="ansObr1" min="1" max="5" required><br><br>
   <label for="pamet2">2. Můžeš mě použít k přehrání hudby a data se na mě zapisují pomocí laserového paprsku. A říkají mi:</label>
   <select id="pamet2" name="pamet2">
-    <script>allOptions()</script>
+    <script>allbuttons()</script>
   <label for="ansObr2">Obrázek číslo:</label>
   <input type="number" id="ansObr2" name="ansObr2" min="1" max="5" required><br><br>
   <label for="pamet3">3. Pravděpodobně mě najdeš uvnitř svého notebooku. Jsem tižší a rychlejší než můj předchůdce, proto si za mě taky musíš připlatit. </label>
   <select id="pamet3" name="pamet3">
-    <script>allOptions()</script>
+    <script>allbuttons()</script>
   <label for="ansObr3">Obrázek číslo:</label>
   <input type="number" id="ansObr3" name="ansObr3" min="1" max="5" required><br><br>
   <label for="pamet4">4. Jsem uložen uvnitř počítače. Skládám se z několika kotoučů, zapisuju data pomocí magnetického pole. 
     Poslední dobou se mě snaží nahradit, ale pořád jsem ta nejlevnější varianta. </label>
   <select id="pamet4" name="pamet4">
-    <script>allOptions()</script>
+    <script>allbuttons()</script>
   <label for="ansObr4">Obrázek číslo:</label>
   <input type="number" id="ansObr4" name="ansObr4" min="1" max="5" required><br><br>
   <label for="pamet5">5. Data se na mě ukládají pomocí elektrických obvodů. Myslím, že už jsme se spolu nejspíš setkali. Určitě víš, že mi říkají:  </label>
   <select id="pamet5" name="pamet5">
-    <script>allOptions()</script>
+    <script>allbuttons()</script>
   <label for="ansObr5">Obrázek číslo:</label>
   <input type="number" id="ansObr5" name="ansObr5" min="1" max="5" required><br><br>
   <input type="reset" id="formReset" value="Začít znovu">
