11bc78f43b5c663db80e5e58754ecc5cb60ca4ae
Zuzana Bohatova <bohatovz@u-pl10.ms.mff.cuni.cz>
2022-12-05 18:21:34 +0100

    hra2 update

--- ZabavnaInformatika/aplikace/hra2.php
+++ ZabavnaInformatika/aplikace/hra2.php
@@ -5,14 +5,34 @@
         document.getElementById("bubbleHardw").remove();
         document.getElementById("hra2zacniHru").remove();
         document.getElementById("robHardw").remove();
+        textHardw.innerHTML += `<div class="hra2container">
+            <img src="../../pictures/drawing01.png" alt="Motherboard">
+            <button id="hra2pci">1</button>
+            <button id="hra2cpu">2</button>
+            <button id="hra2bios">3</button>
+            <button id="hra2disk">4</button>
+            <button id="hra2chipset">5</button>
+            <button id="hra2cards">6</button>
+            <button id="hra2inout">7</button>
+            <div>`;
+        sidebar.innerHTML = `<div class="components"><button>RAM</button>
+            <button>Procesor (CPU)</button>
+            <button>Bios</button>
+            <button>HDD nebo SSD</button>
+            <button>North-South bridge chipset</button>
+            <button>Síťové, zvukové, grafické karty</button>
+            <button>Ethernet, USB, ...</button>
+            </div>`;
+        document.getElementById("sidebar").style.margin = "50px 20px 0px 0px ";
     }
+
 </script>
 <style type="text/css">
     #hardware {
         display:grid;
         grid-template-columns: 70% 5% auto;
         grid-template-areas: 
-            "text . robot ";
+            "text . sidebar ";
         font-size: 18px;
         margin-left: 10px;
     }
@@ -21,8 +41,8 @@
         grid-area: text;
     }
 
-    #robHardw {
-        grid-area: robot;
+    #sidebar {
+        grid-area: sidebar;
         margin-top: 20px;
     }
 
@@ -68,8 +88,58 @@
     }
 
 
-    #hra2zacniHru:hover{
+    #hra2zacniHru:hover, .components button:hover{
         border: 2px solid #FF4F00;
+    }
+
+    /* Container needed to position the button. Adjust the width as needed */
+    .hra2container {
+        position: relative;
+        grid-area: text;
+    }
+
+    /* Make the image responsive */
+    .hra2container img {
+        width: 100%;
+        height: auto;
+    }
+
+    /* Style the button and place it in the middle of the hra2container/image */
+    .hra2container #hra2pci {
+        position: absolute;
+        top: 40%;
+        left: 30%;
+        transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        background-color: transparent;
+        color: white;
+        font-size: 25px;
+        padding: 5px 50px;
+        border: none;
+        cursor: pointer;
+        font-weight: bold;
+    }
+
+    .hra2container #pci:hover {
+        border: 1px solid #E97451;
+        background-color: #FBCEB1
+    }
+
+    .har2container
+
+    .components button {
+        display: block;
+        background-color: #FBCEB1 ;
+        border: 2px solid #FBCEB1 ;
+        border-radius:20px;
+        color: #FF4F00;
+        float: right;
+        font-size: 18px;
+        font-weight: bold;
+        margin-top: 20px;
+        padding: 15px 25px;
+        text-align: center;
+        width: 200px;
     }
 
 </style>
@@ -84,6 +154,7 @@
 </div>
 <button id="hra2zacniHru" onclick="hra2zacni()">Začni hru</button>
 </div>
+<div id="sidebar">
 <img id="robHardw" src="../../pictures/rob04.png" alt="Robot4" width="290">
-
+</div>
 </div>
