4c3dc9abda69a17d7084a459b0f1c3441fd67ddd
Bohatová Zuzana <941-bohatovz@users.noreply.localhost>
2023-10-23 08:47:10 +0000

    Update hry.php

--- ZabavnaInformatika/aplikace/hry.php
+++ ZabavnaInformatika/aplikace/hry.php
@@ -3,6 +3,7 @@
 </script>
 <h2>Hry</h2>
 <ul>
+    <li><a href="hra1">Zvládneš dojít do cíle? aneb cesta grafem</a></li>
     <li><a href="hra2">Umíš správně zapojit jednotlivé komponenty tvého počítače?</a></li>
     <li><a href="hra3">Orientuješ se ve vnějších pamětích?</a></li>
     <li><a href="hra4">Zvládáš převody mezi soustavami?</a></li>
