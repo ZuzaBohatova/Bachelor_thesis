b56844456f9b95a682cec9c7ff0fc646a36d92e8
Bohatová Zuzana <941-bohatovz@users.noreply.localhost>
2023-11-15 14:47:09 +0000

    Update index.php

--- ZabavnaInformatika/aplikace/index.php
+++ ZabavnaInformatika/aplikace/index.php
@@ -1,5 +1,6 @@
 <?php
-    
+    require_once "router.php"; 
+
     function main()
     {
         if (!isset($_SERVER['PATH_INFO']))
@@ -9,50 +10,10 @@
         }
         
         $url = explode('/', substr($_SERVER['PATH_INFO'], 1));
-       
-        switch ($url[0])
-        {
-            case "":
-            case "uvod":
-                require_once __DIR__ . '/uvod.php';
-                break;
-            case "grafy":
-                require_once __DIR__ . '/grafy.php';
-                break;
-            case "hardware":
-                require_once __DIR__ . '/hardware.php';
-                break;
-            case "site":
-                require_once __DIR__ . '/site.php';
-                break;
-            case "ukladani-dat":
-                require_once __DIR__ . '/ukladani-dat.php';
-                break;   
-            case "ciselne-soustavy":
-                require_once __DIR__ . '/ciselne-soustavy.php';
-                break; 
-            case "hra3":
-                require_once __DIR__ . '/hra3Novy.php';
-                break; 
-            case "hra1":
-                require_once __DIR__ . '/hra1.php';
-                break;
-            case "hra4":
-                require_once __DIR__ . '/hra4Novy.php';
-                break;
-            case "hra2":
-                require_once __DIR__ . '/hra2.php';
-                break;
-            case "hry":
-                require_once __DIR__ . '/hry.php';
-                break;  
-            default:
-                error(404, "Page not found");
-                break;
-        }
+        return route($url);
+        
     }
-
     require_once __DIR__ . '/header.php';
-    main();
+    require_once __DIR__ . main();
     require_once __DIR__ . '/footer.php';
 ?>
